7024da09722876c5bc03bd793a7bcdf3a6ac555c
Ankush Tagore <ankushtagore@outlook.com>
2020-05-06 17:27:43 +0530

    Third Commit

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
-    path('tweets', tweet_list_view)
+    path('tweets', tweet_list_view),
+    path('create-tweet', tweet_create_view),
 ]
